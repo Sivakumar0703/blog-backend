92ef26c67f50c93c544d88dd4cbefdcd98841260
sivakumar0703 <ssivakumar358@gmail.com>
2024-04-13 22:48:36 +0530

    adding-server-address

--- Controllers/image.controller.js
+++ Controllers/image.controller.js
@@ -3,7 +3,7 @@ import path from "path";
 // post request
 export const uploadImage =  async(req,res) => {
     try {
-        const url = `${process.env.URL}/blog-images/${req.file.fieldname}_${path.parse(req.file.originalname).name}${path.extname(req.file.originalname)}`
+        const url = `${process.env.SERVER_ADDRESS}/blog-images/${req.file.fieldname}_${path.parse(req.file.originalname).name}${path.extname(req.file.originalname)}`
         res.status(200).json({message:'success',url})
     } catch (error) {
        res.status(500).json({message:'error occured during file upload'})  
